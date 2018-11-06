8a58d3a55e8802bf0e02dd871e88b0aa662c5f8a
austinminich <austin.minich@gmail.com>
2018-11-05 17:38:04 -0800

    Fixed strip function

--- training_test.py
+++ training_test.py
@@ -3,10 +3,6 @@ import os, json, re, string
 # Path to directory containing all training data
 path_to_json = os.path.dirname(os.path.abspath(__file__))
 path_to_json += "\\Sample Data\\"
-#path_to_json = 'C:\\Users\\Alex\\virtualenvironment\\421\\Sample Data\\'
-print(path_to_json)
-
-exit()
 
 def load_training_data(data_path):
     
@@ -23,17 +19,15 @@ def load_training_data(data_path):
         with open(path_to_json + file) as json_file:
             data = json.load(json_file)
             for p in data:
-                train_texts.append(p['content'])
+                train_texts.append(p['content'].lower())
                 train_labels.append(p['category'])
 
     return((train_labels, train_texts))
 
 labels, contents = load_training_data(path_to_json)
 
-def clean_up_data(array):
-
-    punctuationNoPeriod = "[" + re.sub("\.", "", string.punctuation) + "]"
-    array = re.sub(punctuationNoPeriod, "", array)
+def stripPunctuation(s):
+    return ''.join(ch for ch in s if ch not in string.punctuation)
 
 unique_categories = []
 
@@ -43,9 +37,10 @@ for x in labels:
 #print(unique_categories)
 
 for x in contents:
-    clean_up_data(x)
+    x = stripPunctuation(x)
+    #print(x)
+
 
 bigrams = [b for l in contents for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
-
-#print(bigrams)
+print(bigrams[100])
 
