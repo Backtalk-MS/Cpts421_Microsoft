bd5ec85dc1c1de70a626ecbd4f82496fac295dc8
austinminich <austin.minich@gmail.com>
2018-11-05 19:24:22 -0800

    Updated tokenization to use ntlk library

--- training_test.py
+++ training_test.py
@@ -1,5 +1,7 @@
-import os, json, re, string
-from string import punctuation
+import os, json, re, string, nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+from collections import Counter
 
 # Path to directory containing all training data
 path_to_json = os.path.dirname(os.path.abspath(__file__))
@@ -20,12 +22,27 @@ def load_training_data(data_path):
         with open(path_to_json + file) as json_file:
             data = json.load(json_file)
             for p in data:
-                train_texts.append(p['content'].lower())
-                train_labels.append(p['category'])
+                if(p['category'] == 'msoffice'):#Only from one category
+                    train_texts.append(p['content'].lower())
+                    train_labels.append(p['category'])
 
     return((train_labels, train_texts))
 
 labels, contents = load_training_data(path_to_json)
+
+def setupData(trainContents, trainLabels):
+    bigrams = []
+    token = list()
+    counter = 0
+    for docContents in trainContents:
+        token += nltk.word_tokenize(docContents)
+        bigrams = ngrams(token, 2)
+    for key, value in Counter(bigrams).items():
+        if value >= 250:
+            print(key)
+            counter += 1
+            print("Found " + str(counter) + " having atleast 250")
+    #print(Counter(bigrams))
 
 # Function to strip punctuation
 def strip_punctuation(s):
@@ -42,8 +59,14 @@ for x in labels:
 for x in range(len(contents)):
     contents[x] = strip_punctuation(contents[x])
 
-#bigrams = [b for l in contents for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
+setupData(contents, labels)
+exit()
+
 
 bigrams = [b for l in contents for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
-print(bigrams[100])
+unigrams = []
+for string in contents:
+    unigrams.append(string.split(" "))
+#print(bigrams)
+print(unigrams)
 
